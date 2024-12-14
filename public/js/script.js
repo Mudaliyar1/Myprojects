b6ab00339de774af4e636a4a1aa2714ead07b326
Vijay <vijaymudaliyar224@gmail.com>
2024-12-13 21:58:17 -0800

    dark and night mode added

--- public/js/script.js
+++ public/js/script.js
@@ -3,10 +3,26 @@ function ham_open(){
     open.style.display="block";
 }
 
-
-
-
 function ham_close(){
     let close=document.getElementById("navbarmobile");
     close.style.display="none";
+}
+
+function lightMode(){
+    let light=document.getElementById("indexMain");
+    let lightIcon=document.getElementById("lightMode");
+    let darkIcon=document.getElementById("darkMode");
+    light.style.backgroundColor="white";
+    light.style.color="black";
+    lightIcon.style.display="none";
+    darkIcon.style.display="block";
+}   
+function darkMode(){
+    let dark=document.getElementById("indexMain");
+    let darkIcon=document.getElementById("darkMode");
+    let lightIcon=document.getElementById("lightMode");
+    dark.style.backgroundColor="black";
+    darkIcon.style.display="none";
+    lightIcon.style.display="block";
+
 }
