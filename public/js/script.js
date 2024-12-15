ba82f0b1b254c9ba707ac1b7713a126fc7716715
Vijay <vijaymudaliyar224@gmail.com>
2024-12-14 22:18:17 -0800

    js changes

--- public/js/script.js
+++ public/js/script.js
@@ -16,18 +16,33 @@ function lightMode(){
     let light=document.getElementById("indexMain");
     let lightIcon=document.getElementById("lightMode");
     let darkIcon=document.getElementById("darkMode");
+    let backToTop=document.getElementById("backToTop");
+    let backTo=document.getElementsByClassName("backTo");
+    
     light.style.backgroundColor="white";
     light.style.color="black";
     lightIcon.style.display="none";
     darkIcon.style.display="block";
+    backToTop.style.backgroundColor="black";
+
+    for(let i=0;i<backTo.length;i++){
+        backTo[i].style.backgroundColor="white";
+    }
 }   
 function darkMode(){
     let dark=document.getElementById("indexMain");
     let darkIcon=document.getElementById("darkMode");
     let lightIcon=document.getElementById("lightMode");
+    let backToTop=document.getElementById("backToTop");
+    let backTo=document.getElementsByClassName("backTo");
+
     dark.style.backgroundColor="black";
     dark.style.color="white";
     darkIcon.style.display="none";
     lightIcon.style.display="block";
+    backToTop.style.backgroundColor="white";
 
+    for(let i=0;i<backTo.length;i++){
+        backTo[i].style.backgroundColor="black";
+    }
 }
